e2adfced67f8e6d5592d52039d4691cb044970c8
jilu <jilutech@gmail.com>
2023-01-25 22:50:07 -0800

    sample

--- app/src/main/java/com/example/pagination/MainActivity.kt
+++ app/src/main/java/com/example/pagination/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PaginationTheme {
-567890-
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
